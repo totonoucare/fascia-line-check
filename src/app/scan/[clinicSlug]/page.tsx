d73d0d9f9db19b7cef6a114d3bbb8dbc804e3d37
totonoucare <novelinks9@gmail.com>
2026-06-27 05:41:25 +0900

    Add files via upload

--- src/app/scan/[clinicSlug]/page.tsx
+++ src/app/scan/[clinicSlug]/page.tsx
@@ -1,1 +1,7 @@
+import { CheckWidget } from "@/components/CheckWidget";
+import { getClinicBySlug } from "@/lib/supabaseAdmin";
 
+export default async function ScanPage({ params }: { params: { clinicSlug: string } }) {
+  const clinic = await getClinicBySlug(params.clinicSlug);
+  return <CheckWidget clinic={clinic} />;
+}
