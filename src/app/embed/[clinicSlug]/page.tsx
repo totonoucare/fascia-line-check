76dfb2a535751d830225210aeed7cfab39fcb6a1
totonoucare <novelinks9@gmail.com>
2026-06-28 12:36:20 +0900

    Add files via upload

--- src/app/embed/[clinicSlug]/page.tsx
+++ src/app/embed/[clinicSlug]/page.tsx
@@ -1,1 +1,11 @@
+import { CheckWidget } from "@/components/CheckWidget";
+import { getClinicBySlug } from "@/lib/supabaseAdmin";
 
+export default async function EmbedPage({ params }: { params: { clinicSlug: string } }) {
+  const clinic = await getClinicBySlug(params.clinicSlug);
+  return (
+    <div className="bg-transparent">
+      <CheckWidget clinic={clinic} embed />
+    </div>
+  );
+}
